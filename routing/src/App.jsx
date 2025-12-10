d507314dc84ce94819c91d2c6113e49e43acd75d
deepan-crypto <deepang467@gmail.com>
2025-12-10 12:09:07 +0530

    OUtlet

--- routing/src/App.jsx
+++ routing/src/App.jsx
@@ -8,6 +8,15 @@ function App() {
 
   return (
     <div className="App">
+      <header>
+        <img src=""></img>
+        <div>
+          <NavLink to={"/"}>Home </NavLink>
+          <NavLink to={"/about"}>About </NavLink>
+                    <NavLink to={"/cart"}>Cart</NavLink>
+                    <NavLink to={"/search"}>Search</NavLink> 
+        </div>
+      </header>
       <Outlet/>
     </div>
 
