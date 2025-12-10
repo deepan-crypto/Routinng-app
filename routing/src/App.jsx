17c5ceab6df7513333b9798cd24b814693565559
deepan-crypto <deepang467@gmail.com>
2025-12-10 09:51:13 +0530

    App.jsx

--- routing/src/App.jsx
+++ routing/src/App.jsx
@@ -1,35 +1,38 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [val, setVal] = useState(0); 
+
+  useEffect(() => {
+    console.log("UseEffect-Run Once after initial Render");
+  }, []);
+
+  useEffect(() => {
+    console.log("UseEffect-with Array of Dependencies");
+  }, [val]);
+
+  useEffect(() => {
+    console.log("UseEffect-Run on every Render");
+  });
+
+  useEffect(() => {
+    if (val < 0) {
+      setTimeout(() => {
+        setVal(0);
+      }, 2000);
+    }
+  }, [val]); 
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="App">
+      <h1>Learning react</h1>
+      
+    </div>
+  );
 }
 
 export default App
