2a3c86beec9c50e55bb879e4c4e89ecb18d08dc5
deepan-crypto <deepang467@gmail.com>
2025-12-10 12:00:09 +0530

    update

--- routing/src/index.js
+++ routing/src/index.js
@@ -3,12 +3,21 @@ import App from './App.jsx';
 import ReactDOM from 'react-dom/client';
 import { CreateBrowserRouter,RouterProvider } from 'react-router-dom';
 import Home from './pages/Home.js';
+import App from './App.jsx';
+import Search from './pages/Search.js';
+import About from './pages/About.js';
+import Cart from './pages/Cart.js';
+import './index.css';
+
+
+
 
 const routerVariables=CreateBrowserRouter([
     {       
         path:"/",
         element:<App/>,
-    },
+        children:[
+    
     {
         path:"/home",
         element:<Home/>,
@@ -21,4 +30,21 @@ const routerVariables=CreateBrowserRouter([
         path:"/about",
         element:<About/>
     },
-    
+    {
+        path:"/cart",
+        element:<Cart/>
+
+    },
+],
+},
+],
+);
+
+
+const root=ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+    <React.StrictMode>
+        <RouterProvider router={routerVariables}/>
+    </React.StrictMode>
+);
