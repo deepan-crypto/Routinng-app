86a1edd396f5cf30b8857807d894a37aac75332c
deepan-crypto <deepang467@gmail.com>
2025-12-10 12:02:31 +0530

    App.jsx

--- routing/src/App.jsx
+++ routing/src/App.jsx
@@ -2,12 +2,13 @@ import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Outlet } from 'react-router-dom';
 
 function App() {
 
   return (
     <div className="App">
-      
+      <Outlet/>
     </div>
 
   );
